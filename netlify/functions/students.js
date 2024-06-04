92f6bfff32614388546b5b84d9dc4c0188558736
bryanpalau <bryanpalau@gmail.com>
2024-06-04 13:52:43 +0800

    Update students.js

--- netlify/functions/students.js
+++ netlify/functions/students.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const db = new sqlite3.Database(path.resolve(__dirname, '../../schema.db'));
+const db = new sqlite3.Database(path.resolve(__dirname, '..', '..', 'schema.db'));
 
 exports.handler = async (event, context) => {
   const studentId = event.path.split('/').pop();
